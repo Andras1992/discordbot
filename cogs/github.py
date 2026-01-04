05a69309bb5a372da1e4d42f226ac5fde93ee373
Andras <benjamin.knippenberg@gmail.com>
2026-01-04 23:34:49 +0100

    Init Cogs
    First entry of cogs to keep the main.py small

---
+++ cogs/github.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+
+
+class Github(commands.Cog):
+    def __init__(self, bot: discord.bot):
+        self.bot = bot
+
+@slash_command(description="Sende GitHub Link")
+async def github(ctx):
+    await ctx.respond(f"Hallo der Github Link ist https://github.com/Andras1992/discordbot !")
+
+
+def setup(bot: discord.bot):
+    bot.add_cog(github(bot))
--- cogs/main.py
+++ cogs/main.py
@@ -13,7 +13,7 @@ async def on_ready():
     print(f'{bot.user} ist Online!')
 
 
-for filename in os.listdir("cogs"):
+for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
